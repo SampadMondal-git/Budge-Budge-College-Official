2a4f978670fe8cff94a71b9d6966303355aafffc
Sampad Mondal <sampadm310@outlook.com>
2025-02-13 19:17:46 +0530

    scroll effect added

--- src/home page/body.jsx
+++ src/home page/body.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './body.css'
-import {Slideshow} from '../home page/slideshow.jsx'
-import {Facilities} from '../home page/facilities.jsx'
+import { motion } from 'framer-motion'
+import { Slideshow } from '../home page/slideshow.jsx'
+import { Facilities } from '../home page/facilities.jsx'
 import { Offer } from '../home page/offer.jsx'
 import { College } from '../home page/college.jsx'
 import { Courses } from '../home page/courses.jsx'
@@ -9,17 +10,94 @@ import { Teachers } from '../home page/teachers.jsx'
 import { Quote } from '../home page/quote.jsx'
 import { Campus } from '../home page/campus.jsx'
 
+const fadeInUp = {
+  initial: {
+    y: 40,
+    opacity: 0
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.22, 1, 0.36, 1]
+    }
+  },
+  exit: {
+    y: 40,
+    opacity: 0,
+    transition: {
+      duration: 0.5
+    }
+  }
+}
+
 export function Body() {
   return (
     <>
-    <Slideshow />
-    <Facilities />
-    <Offer />
-    <College />
-    <Courses />
-    <Teachers />
-    <Quote />
-    <Campus />
+      <Slideshow />
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <Facilities />
+      </motion.div>
+
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <Offer />
+      </motion.div>
+
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <College />
+      </motion.div>
+
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <Courses />
+      </motion.div>
+
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <Teachers />
+      </motion.div>
+
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <Quote />
+      </motion.div>
+
+      <motion.div
+        initial="initial"
+        whileInView="animate"
+        exit="exit"
+        viewport={{ amount: 0.4 }}
+        variants={fadeInUp}>
+        <Campus />
+      </motion.div>
     </>
   )
 }
