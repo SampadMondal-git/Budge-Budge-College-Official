2c0cd9b4a05cbf066ec9cb7e56feb811d39fc2cb
Sampad Mondal <sampadm310@outlook.com>
2025-02-13 19:43:28 +0530

    navlink.jsx modified

--- src/home page/navlink.jsx
+++ src/home page/navlink.jsx
@@ -28,14 +28,14 @@ export function Navlink() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href='/'><img src={Logo} alt="Logo" height={"60px"} className="d-inline-block align-text-top logo-css" /></a>
+                    <Link className="navbar-brand" to='/body'><img src={Logo} alt="Logo" height={"60px"} className="d-inline-block align-text-top logo-css" /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link active" aria-current="page" to="/body">Home</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
