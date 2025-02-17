aba30d88073587bd0ce71763206071062b8f3938
Sampad Mondal <sampadm310@outlook.com>
2025-02-17 20:38:58 +0530

    path connected

--- src/home page/navbar.jsx
+++ src/home page/navbar.jsx
@@ -1,4 +1,5 @@
 import myStyles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 import { Navlink } from './navlink.jsx';
 export function Navbar() {
 
@@ -8,18 +9,18 @@ export function Navbar() {
         <div className={myStyles.contact}>
           <div className={myStyles.email}>
             <i className="ri-message-3-fill"></i>
-            <p>Email: budgebudgecollege@gmail.com</p>
+            <p>Email: <a href="mailto:budgebudgecollege@gmail.com">budgebudgecollege@gmail.com</a></p>
           </div>
 
           <div className={myStyles.phone}>
             <i className="ri-phone-fill"></i>
-            <p>Phone: 033 24701454 | 033 24805168</p>
+            <p>Phone: <a href="tel:03324701454">033 24701454</a> | <a href="tel:03324805168">033 24805168</a></p>
           </div>
         </div>
         <div className={myStyles.button}>
           <a href="https://wbcap.in/"><button>Apply Now</button></a>
           <a href="https://cmserp.in/cms/?curl=bbc&cmsof=1"><button>Pay Your Fees</button></a>
-          <a href="/"><button>Notification</button></a>
+          <Link to="/notification"><button>Notification</button></Link>
         </div>
       </div>
 
