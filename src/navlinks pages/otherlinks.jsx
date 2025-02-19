a1bbdf1ed0d31f2367101fd2b6454b5a4892c046
Sampad Mondal <sampadm310@outlook.com>
2025-02-19 18:53:33 +0530

    create otherlinks.jsx & push code

---
+++ src/navlinks pages/otherlinks.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import myStyles from './otherlinks.module.css'
+import { Link } from 'react-router-dom'
+export function Otherlinks() {
+    return (
+      <div className={myStyles.container}>
+            <h2 className={myStyles.section_title}>Other Links</h2>
+        <div className={myStyles.governing_staffs}>
+        <div className={myStyles.staffs}>
+          <h5>NAAC</h5>
+          <p><Link to="/naac">Visit NAAC</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>IQAC</h5>
+          <p><Link to="/iqac">Visit IQAC</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>Student's Zone</h5>
+          <p><Link to="/studentszone">Visit Student's Zone</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>Activities</h5>
+          <p><Link to="/activities">Visit Activities</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>Alumni</h5>
+          <p><Link to="/alumni">Visit Alumni</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>Feedback</h5>
+          <p><Link to="/feedback">Visit Feedback</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>Image Gallery</h5>
+          <p><Link to="/imagegallery">Visit Image Gallery</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>NSOU (PG Courses)</h5>
+          <p><Link to="/nsougpcourses">Visit NSOU (PG Courses)</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>Library</h5>
+          <p><Link to="/library">Visit Library</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>OPAC</h5>
+          <p><Link to="/opac">Visit OPAC</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>RTI</h5>
+          <p><Link to="/rti">Visit RTI</Link></p>
+        </div>
+        <div className={myStyles.staffs}>
+          <h5>NIRF</h5>
+          <p><Link to="/nirf">Visit NIRF</Link></p>
+        </div>
+        </div>
+      </div>
+    )
+  }
