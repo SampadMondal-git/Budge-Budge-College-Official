49c04e0e1fac1fd5af1a14105864298767f6d8bc
Sampad Mondal <sampadm310@outlook.com>
2025-02-22 01:05:40 +0530

    JSX modified

--- src/navlinks pages/contact.jsx
+++ src/navlinks pages/contact.jsx
@@ -9,25 +9,27 @@ export function Contact() {
                 <div className={myStyle.grid}>
                     <div className={myStyle.card}>
                         <h3>Address</h3>
-                        <div className={myStyle.contact_info}>
+                        <div className={myStyle.point}>
                             <p>7, Deshbandhu Chittaranjan Road, Budge Budge, 24 Parganas (South), Kolkata - 700137</p>
                         </div>
                     </div>
 
                     <div className={myStyle.card}>
                         <h3>Telephone</h3>
-                        <div className={myStyle.contact_info}>
-                            <p>033 24701454</p>
-                            <p>033 24805168</p>
+                        <div className={myStyle.point}>
+                            <p><span className={myStyle.highlight}>Telephone: </span>
+                                <a href="tel:03324701454">033 24701454</a>,
+                                <a href="tel:03324805168">033 24805168</a>
+                            </p>
                         </div>
                     </div>
 
                     <div className={myStyle.card}>
                         <h3>Email</h3>
-                        <div className={myStyle.contact_info}>
-                            <a href="mailto:budgebudgecollege@gmail.com">
-                                budgebudgecollege@gmail.com
-                            </a>
+                        <div className={myStyle.point}>
+                            <p><span className={myStyle.highlight}>Email: </span>
+                                <a href="mailto:budgebudgecollege@gmail.com">budgebudgecollege@gmail.com</a>
+                            </p>
                         </div>
                     </div>
                 </div>
