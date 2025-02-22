bd59dac921c16eb50db905545fe77418c6555d47
Sampad Mondal <sampadm310@outlook.com>
2025-02-22 19:07:54 +0530

    code created

---
+++ src/navlinks pages/nirf.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import myStyles from './nirf.module.css'
+//nirf pdf
+import nirfPdf from '../Pdf files/NIRF/Budge-Budge-College-20250215.pdf';
+export function Nirf() {
+    return (
+        <>
+            <div className={myStyles.container}>
+                <h2 className={myStyles.section_title}>Right to Information</h2>
+
+                <div className={myStyles.grid}>
+                    <div className={myStyles.card}>
+                        <h3>National Institutional Ranking Framework - NIRF</h3>
+                        <div className={myStyles.point}>
+                            <p className={myStyles.highlight}>Download NIRF Document: <a href={nirfPdf} download="Budge-Budge-College-20250215.pdf">Budge-Budge-College-20250215</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
