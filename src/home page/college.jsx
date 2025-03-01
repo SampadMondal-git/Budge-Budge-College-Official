08695cb9b1c9abfbd485d076f4dd0acb91954e9e
Sampad Mondal <sampadm310@outlook.com>
2025-03-01 10:17:08 +0530

    courses container added

--- src/home page/college.jsx
+++ src/home page/college.jsx
@@ -14,6 +14,28 @@ export function College() {
                     <p>Budge Budge College, established on December 10, 1971, initially operated from a different location before moving to its current site near Budge Budge Railway Station. Founded by renowned writer Narayan Ganguly, the college has been pivotal in advancing education in the Kolkata suburbs, particularly among middle and lower-middle-class families. With a diverse student body, including many female students, the college has become a significant educational institution, contributing to the development of well-rounded, responsible citizens.</p>
                 </div>
             </div>
+
+                        <div className={myStyles.courses_container}>
+                <div className={`${myStyles.certified_teachers} ${myStyles.item}`}>
+                    <h1>45+</h1>
+                    <p>Certified Teachers</p>
+                </div>
+
+                <div className={`${myStyles.students} ${myStyles.item}`}>
+                    <h1>2000+</h1>
+                    <p>Students</p>
+                </div>
+
+                <div className={`${myStyles.courses} ${myStyles.item}`}>
+                    <h1>25+</h1>
+                    <p>Courses</p>
+                </div>
+
+                <div className={`${myStyles.awards_won} ${myStyles.item}`}>
+                    <h1>20+</h1>
+                    <p>Awards Won</p>
+                </div>
+            </div>
         </>
     )
 }
