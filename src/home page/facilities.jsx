256a9fc94a9bbcdb8fd73a0e3cbeef174ae5316c
Sampad Mondal <sampadm310@outlook.com>
2025-03-01 10:23:29 +0530

    JSX modified

--- src/home page/facilities.jsx
+++ src/home page/facilities.jsx
@@ -8,24 +8,24 @@ export function Facilities() {
     return (
         <>
             <div className={myStyles.boxes}>
-                <div className={myStyles.box1}>
+                <div className={myStyles.box}>
                     <img src={teacher} alt="Certified Teachers" />
                     <h1>Certified Teachers</h1>
                     <p>We have a team of certified teachers who are committed to providing excellent learning experiences for
                         our students.</p>
                 </div>
-                <div className={myStyles.box2}>
+                <div className={myStyles.box}>
                     <img src={special} alt="Special Education" />
                     <h1>Special Education</h1>
                     <p>We have a dedicated special education team who provide support and guidance to students with special
                         needs.</p>
                 </div>
-                <div className={myStyles.box3}>
+                <div className={myStyles.box}>
                     <img src={book} alt="Book & Library" />
                     <h1>Book & Library</h1>
                     <p>We have a well-stocked library with a collection of books, manuscripts, and reference materials.</p>
                 </div>
-                <div className={myStyles.box4}>
+                <div className={myStyles.box}>
                     <img src={sport} alt="Sports Club" />
                     <h1>Sports Club</h1>
                     <p>We have a dedicated sports club that provides a variety of activities, sports, and recreational
