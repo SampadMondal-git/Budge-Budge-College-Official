8896ec2fd0abbbaae41aeea98d12605d0a0152a0
Sampad Mondal <sampadm310@outlook.com>
2025-04-12 23:08:21 +0530

    link error page

--- src/home page/navlink.jsx
+++ src/home page/navlink.jsx
@@ -28,14 +28,14 @@ export function Navlink() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to='/budgebudgecollege'><img src={Logo} alt="Logo" height={"60px"} className="d-inline-block align-text-top logo-css" /></Link>
+                    <Link className="navbar-brand" to='/'><img src={Logo} alt="Logo" height={"60px"} className="d-inline-block align-text-top logo-css" /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/budgebudgecollege" onClick={handleLinkClick}>Home</Link>
+                                <Link className="nav-link active" aria-current="page" to="/" onClick={handleLinkClick}>Home</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
